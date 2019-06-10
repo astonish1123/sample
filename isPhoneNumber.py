64a5bddb264eec7ece68bfe66e85edb485164fb0
Ethan Nakashima <h.nakashima.ny@gmail.com>
2019-06-09 21:36:21 -0700

    modified the last 6 lines in def

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -16,6 +16,12 @@ def is_phone_number(text):
             if not text[i].isdecimal():
                 return False
         return True
+    message = "Please call 415-555-1011. To office, 415-555-9999"
+    for i in range(len(message)):
+        chunk = message[i:i+12]
+        if is_phone_number(chunk):
+            print('The phone number is found: ' + chunk)
+    print('Complete!')
 
 print('Phone number is 415-555-4242')
 print(is_phone_number('415-555-4242'))
